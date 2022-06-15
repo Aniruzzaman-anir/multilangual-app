0af394dd8d32f041845c46511b9fb59d4174903a
AL EMRAN <anirdms@gmail.com>
2022-06-15 10:00:58 +0600

    spanish language support

---
+++ src/lang/es.js
@@ -0,0 +1,28 @@
+export default {
+    "brand":"Soft UI Dashboard",
+    "menu":{
+        "dashboard":"Tablero",
+        "profile":"Perfil",
+        "signup": "Inscribirse",
+        "signin":  "Iniciar sesión"
+    },
+    "builder_btn": "Constructor en línea",
+    "download_btn": "Descarga gratis",
+    "welcome_msg": "Bienvenido de nuevo",
+    "signin_instruction": "Ingrese su correo electrónico y contraseña para iniciar sesión",
+    "email_label":"Correo electrónico",
+    "password_label": "Clave",
+    "remember_label": "Recuerde Sr.",
+    "signin_btn": "Iniciar sesión",
+    "no_account_ques": "No tienes una cuenta?",
+    "signup_link": "Registro",
+    "footer":{
+        "company_link": "Institución",
+        "aboutus_link":"Sobre nosotros",
+        "team_link": "Equipo",
+        "products_link": "productos",
+        "blog_link" : "Blog",
+        "pricing_link" : "Precios"
+    }
+
+}
